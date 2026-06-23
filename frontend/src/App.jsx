e53984ba64d6e2b95166a4743578e5c62d4a1a24
kshoqw-prog <kshoqw@gmail.com>
2026-06-24 08:37:29 +1000

    adminp1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -194,8 +194,7 @@ function AppInner() {
       {toast && <Toast message={toast} onDismiss={() => setToast('')} />}
     </div>
   );
-}
-
+}//12312312
 export default function App() {
   return (
     <AuthProvider>
